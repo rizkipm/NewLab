a593a79cf1d72126559c518d9592c1ebdcd8787b
Programmer Muslim <rizki@imastudio.co.id>
2018-02-06 17:55:28 +0700

    Initial commit

---
+++ app/src/main/java/imastudio/id/co/butterknife/MainActivity.java
@@ -0,0 +1,26 @@
+package imastudio.id.co.butterknife;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.txtView)
+    TextView txtView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.txtView)
+    public void onViewClicked() {
+    }
+}
